7781ced6a4534e3d600532fa406e8df21d52ee1d
Alma Benitez <alma.bntz@gmail.com>
2022-11-01 18:54:25 -0500

    clear function added to search bar

--- client/src/components/searchBar.js
+++ client/src/components/searchBar.js
@@ -1,17 +1,20 @@
 import React, { useEffect, useState } from 'react';
 import "./searchBar.css";
 import SearchIcon from '@mui/icons-material/Search';
-
+import CloseIcon from '@mui/icons-material/Close';
 
 function SearchBar({placeholder, data}) {
     //use states and logic 
-    const [filteredData, setFilteredData] = useState([]);
+    const [filteredData, setFilteredData] = useState([]); //user input to search bar
+    const [wordEntered, setWordEntered] = useState("");// clears key terms used in searchbar
+    
     //Event handler for user typing into search bar
     const handleFilter = (e) => {
         const searchWord = e.target.value
         const newFilter = data.filter((value) =>{
             return value.name.toLowerCase().includes(searchWord.toLowerCase());
         });
+        //hides the window that appears below the search bar if input is empty
         if (searchWord === ""){
             setFilteredData([])
         }else{
@@ -19,12 +22,22 @@ function SearchBar({placeholder, data}) {
         }
     };
 
+    //Event handler for clearing a search term inside the search bar
+    const clearInput = () => {
+       setFilteredData([]); 
+    }
+
     return (
         <div className='search'>
             <div className='searchInputs'>
                 <input type="text" placeholder={placeholder} onChange={handleFilter}/>
                     <div className='searchIcon'>
-                    <SearchIcon/>
+                        {filteredData.length === 0 ? (
+                             <SearchIcon/> 
+                             ) : (
+                                 <CloseIcon id="clearBtn" onClick={clearInput} />
+                                 )}
+                    
                     </div>
             </div>
             { filteredData.length != 0 && (
