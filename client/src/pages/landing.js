fedc100e302e15a5bebbf2c9f27e344b114dcd0c
Alma Benitez <alma.bntz@gmail.com>
2022-11-08 16:00:14 -0600

    Created Landing page component

---
+++ client/src/pages/landing.js
@@ -0,0 +1,16 @@
+import React from "react";
+import landing from "../components/landing.js"
+
+const Home = () => {
+    
+    //logic goes here
+
+return (
+    <div>
+        <h1>This is my landing page</h1>
+    </div>
+)
+
+}
+
+export default Home;
