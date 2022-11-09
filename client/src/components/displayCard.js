26ad0384ca23f274a84fa780be1482a51d19aa5f
Alma Benitez <alma.bntz@gmail.com>
2022-11-09 13:30:20 -0600

    images are rendering

--- client/src/components/displayCard.js
+++ client/src/components/displayCard.js
@@ -3,17 +3,26 @@ import { useState } from "react";
 
 const DisplayCard = ({ card }) => {
   //QUALITY OF LIFE CODE
-  // function will make special char appear on page, will pass them through function as string
-  function decodeHtml(html) {
-    var txt = document.createElement("textarea");
-    txt.innerHTML = html;
-    return txt.value;
-  }
-  console.log(card, "this is one card");
+  //function will make special char appear on page, will pass them through function as string
+  // function decodeHtml(html) {
+  //   var txt = document.createElement("textarea");
+  //   txt.innerHTML = html;
+  //   return txt.value;
+  // }
+console.log(card, "this is one card");
+
+//console.log(card.imageURL, "This is my test for image")
+
+console.log(card.rarity, "test for Rarity")
 
   return (
     <div className="display-card">
-      <h2>{JSON.stringify(card.name)}</h2>
+      <h2>{card.name}</h2>
+
+   {card.imageUrl && <img src={card.imageUrl} alt="image"/>}
+
+    
+  
       <p>
         <b>Mana Cost:</b> {JSON.stringify(card.manaCost)}
       </p>
@@ -30,3 +39,5 @@ data is being passed to page ARCHIVE */
 
 //this might work for returning image
 //{decodeHtml(<img src={card.png}/>)}
+//<img src="http://gatherer.wizards.com/Handlers/Image.ashx?multiverseid=547444&type=card"/>
+// <h1>{card.imageUrl}</h1>
