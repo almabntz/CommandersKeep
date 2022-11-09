874d338e78ad5bc63b1271505859ae3dd9655d91
Alma Benitez <alma.bntz@gmail.com>
2022-11-09 00:33:31 -0600

    Log in capturing user data, user can log out

--- client/src/App.js
+++ client/src/App.js
@@ -11,19 +11,31 @@ import Archive from './pages/archive';
 import Home from './pages/home';
 //auth0
 import Auth0NavBar from './auth/nav-bar';
+import {useAuth0} from '@auth0/auth0-react';
+import Loading from "./auth/loading";
+import { Route, Routes, Link } from 'react-router-dom';
 
 
 function App() {
   const [view, setView] = useState("myCollection")
+  //auth0
+  const { isLoading } = useAuth0();
+  const { user } = useAuth0();
+  if (isLoading) {
+    return <Loading />;
+  }
+
   return (
     //Nav Bar Code
-    <div className="App">
+    <div id="app" className="d-flex flex-column h-100">
       <NavBar setView={setView}/>
       {view === 'myCollection' && <Collection/>}
       {view === 'myDecks' && <Decks/>}
       {view === 'archive' && <Archive/>}
       {view === 'home' && <Home />}
-<div>
+      
+<div className="container flex-grow-1">
+  { !user ? <span>Hello from Techtonica</span> : <Dashboard /> }
 <Auth0NavBar/>
 </div>
 
