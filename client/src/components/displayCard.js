2f594f1c7de3d22cb3989a79a49f3d8d5f20498b
Alma Benitez <alma.bntz@gmail.com>
2022-11-10 12:41:51 -0600

    formattted and styled Card Layout

--- client/src/components/displayCard.js
+++ client/src/components/displayCard.js
@@ -1,43 +1,44 @@
-import { useState } from "react";
+import React from "react";
 import "./displayCard.css";
 
 const DisplayCard = ({ card }) => {
- 
-  console.log(card, "this is one card");
-
-  //console.log(card.imageURL, "This is my test for image")
-
-  console.log(card.rarity, "test for Rarity");
+  // const onClick () => {}
+  //THIS WILL COME LATER AND WILL PASS DATA TO MYCOLLECTION
 
   return (
-    <div className="display-card">
-     
-      <div className="card-name">
-      <h2>{card.name}</h2>
+    <div className="card">
+      <div className="card-horizontal">
+        <div className="img-square-wrapper">
+          {card.imageUrl && (
+            <img src={card.imageUrl} alt="image" className="card-img" />
+          )}
+        </div>
+        <div className="card-body">
+          <h2 className="card-title">{card.name}</h2>
+          <p className="card-text">
+            <b>Mana Cost:</b> {JSON.stringify(card.manaCost)}
+            <br></br>
+            <br></br>
+            <b>Flavor Text:</b> {JSON.stringify(card.originalText)}
+            <br></br>
+            <br></br>
+            <b>Resale Value:</b> ${JSON.stringify(card.cmc)}
+          </p>
+        </div>
       </div>
-     
-    <div className="card-img">
-    {card.imageUrl && <img src={card.imageUrl} alt="image" />}
-    </div>
-     
-    <div className="card-mana">
-    <p>
-        <b>Mana Cost:</b> {JSON.stringify(card.manaCost)}
-      </p>
-    </div>
-    
-    <div className="card-text">
-      <p>
-        <b>Flavor Text:</b> {JSON.stringify(card.originalText)}
-      </p>
-    </div>
-    
+      <div className="card-footer w-100 text-muted">
+        <p>
+          <b>legalities:</b> {JSON.stringify(card.legalities)}
+        </p>
+        <button className="card-add-button"> + My Collection </button>
+      </div>
     </div>
   );
 };
 
 export default DisplayCard;
-/* Need to format how data appears here, 
-data is being passed to page ARCHIVE */
 
-
+/*I know i need to take individual card data and pass if from displatcard to collection
+I think i can assign the prop value to each card and have that data post to a table in the back.
+im going to start by looking into tutorials on how to save "favorites" because the concepts 
+are similar*/
