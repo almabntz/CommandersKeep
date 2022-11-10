75643d013a249d9a8793f4b5ff275d84a5a1aa1a
Alma Benitez <alma.bntz@gmail.com>
2022-11-09 18:51:02 -0600

    added post to front end

--- client/src/auth/authentication-button.js
+++ client/src/auth/authentication-button.js
@@ -6,7 +6,11 @@ import { useAuth0 } from '@auth0/auth0-react';
 
 
 const saveUser = (user) => {
-return fetch ()
+return fetch ("postgres://localhost:5432/com_keep",{
+  method: "POST",
+  headers: {"Content-Type": "application/json"},
+  body: JSON.stringify(user),
+})
 }
 
 const AuthenticationButton = () => {
