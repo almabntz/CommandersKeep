7901b53aa18ac9662c641ba9b61eb355bac910ec
Alma Benitez <alma.bntz@gmail.com>
2022-11-17 16:22:42 -0600

    component deckDisplayCard
    passing empty display card to myDeck

---
+++ client/src/components/deckDisplayCard.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+const DeckDisplayCard = () => {
+//logic here
+
+return(
+    <div>
+        display deck things here
+    </div>
+)
+}
+
+export default DeckDisplayCard
--- client/src/pages/myDecks.js
+++ client/src/pages/myDecks.js
@@ -1,21 +1,25 @@
 import React, { useEffect, useState } from 'react';
-
+import DeckDisplayCard from '../components/deckDisplayCard';
 
 const Decks = () => {
 const [myDeck, setMyDeck] = useState([]); //this will hold cards within the deck
     //logic goes here
 const getDeck = async () => {
     const response = await fetch("/user_deck");
-    const storedCollection = await response.json();
-    setMyCollection(storedCollection);
+    const storedDeck = await response.json();
+    setMyDeck(storedDeck);
   };
   useEffect(() => {
-    getCollection();
-  }, [myCollection]);
-}
+    getDeck();
+  }, [myDeck]);
+
     return (
         <div>
+            <div>
             <h1>This is my Decks</h1>
+            <DeckDisplayCard />
+            </div>
+            
         </div>
     )
 };
