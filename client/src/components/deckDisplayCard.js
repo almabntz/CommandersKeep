ace2fb3175a3d79015ce23e227e8d087a620f91c
Alma Benitez <alma.bntz@gmail.com>
2022-11-17 16:53:02 -0600

    Update deckDisplayCard.js
    Backend data for deck table is rendering in frontend

--- client/src/components/deckDisplayCard.js
+++ client/src/components/deckDisplayCard.js
@@ -4,9 +4,29 @@ const DeckDisplayCard = ({displayDeck}) => {
 //logic here
 
 return(
-    <div>
-        
+    <div className="card">
+    <div className="card-horizontal">
+      <div className="img-square-wrapper">
+        {displayDeck.imageurl && (
+          <img src={displayDeck.imageurl} alt="image" className="card-img" />
+        )}
+      </div>
+      <div className="card-body">
+        <h2 className="card-title">{displayDeck.name}</h2>
+        <p className="card-text">
+          <b>Mana Cost:</b> {JSON.stringify(displayDeck.manacost)}
+          <br></br>
+          <br></br>
+          <b>Flavor Text:</b> {JSON.stringify(displayDeck.originaltext)}
+          <br></br>
+          <br></br>
+          <b>Converted Mana Cost:</b> {JSON.stringify(displayDeck.cmc)}
+        </p>
+      </div>
     </div>
+    <div className="card-footer w-100 text-muted">
+    </div>
+  </div>
 )
 }
 
